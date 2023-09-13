d259e3c4ea3113898a0405c1b61cc9ed4d91f7c0
Cha, Jiyong <wldyd2113@gmail.com>
2023-09-13 11:18:54 +0900

    main 메소드 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,9 +2,10 @@ package git_test2;
 
 public class Main {
 
-	public static void main(String[] args) {
-		new Heloo().say();
-
-	}
+    public static void main(String[] args) {
+        Hello h = new Hello();
+        h.say();
+    }
 
 }
+
