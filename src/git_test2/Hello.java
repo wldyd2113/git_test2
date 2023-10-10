07188e60a8a7aff22ab5f1e51d3f882440e6ebf0
Cha, Jiyong <wldyd2113@gmail.com>
2023-10-10 21:57:39 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,12 +1,12 @@
 package git_test2;
-public class Hello {
-      String message; public Hello() {
-           this("안녕하세요"); 
-          }
-           public Hello(String message) { 
-               this.message = message;
-           }
-            public void say() {
-                System.out.println(message);
-                } 
-               }
+ public class Hello {
+     String message; public Hello() {
+         this("안녕하세요"); 
+        }
+         public Hello(String message) { 
+            this.message = message;
+        } 
+        public void say() {
+             System.out.println(message);
+             }
+             }
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,6 +1,8 @@
-package git_test2;
- public class Main {
-     public static void main(String[] args) {
-         Hello hello = new Hello(); hello.say(); 
-        } 
-    }
+package git_test2; 
+public class Main {
+     public static void main(String[] args) { 
+        Hello h = new Hello("hello world");
+         h.say(); 
+        }
+     }
+
