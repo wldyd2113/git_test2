97b7ab4303c618a961a62e5d7fe37944ab662558
Cha, Jiyong <wldyd2113@gmail.com>
2023-10-17 16:50:35 +0900

    Main.java 수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,2 +1,8 @@
-package git_test2; 
-public class Main { public static void main(String[] args) { Hello hello = new Hello(); hello.say(); } }
+package git_test2;
+ public class Main {
+     public static void main(String[] args) { 
+        Hello hello = new Hello();
+         hello.say();
+        }
+    }
+         
