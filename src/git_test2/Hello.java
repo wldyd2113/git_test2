57c3a0972233cb4e89b4d330656eb166085f40ad
Cha, Jiyong <wldyd2113@gmail.com>
2023-10-17 17:02:03 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,14 +1,7 @@
 package git_test2;
 public class Hello {
-	String message; public Hello() {
-		this("안녕하세요");
-		}
-	public Hello(String message) {
-		this.message = message;
-		}
-	
-	public void say() 
-	{
-		System.out.println(message); 
+	 String message = "안녕하세요";
+	 public String say() {
+		 return message; 
 		} 
 	}
--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,6 +3,7 @@ package git_test2;
      public static void main(String[] args) { 
         Hello hello = new Hello();
          hello.say();
+         System.out.println(hello.say());
         }
     }
          
